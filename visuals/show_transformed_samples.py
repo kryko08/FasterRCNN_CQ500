1db02696504b5101d11f55d8d7e04cb5c957c119
Krystof Kopecny <krystof.kope@gmail.com>
2022-05-12 22:14:11 +0200

    Fixed function for showing transformed images

--- visuals/show_transformed_samples.py
+++ visuals/show_transformed_samples.py
@@ -1,63 +1,51 @@
 import os
+import random
 
 import matplotlib.pyplot as plt
 
 from transforms.my_transforms import *
+import torchvision.transforms.functional as TF
 from dataset import HemorrhageDataset
 
-from config import CLASSES, COLOR_MAP
+from config import CLASSES, COLOR_MAP, TRAIN_CSV, TRAIN_DIR
 
-from PIL import Image, ImageDraw
-
-
-cwd = os.getcwd()
-annotations = os.path.join(cwd, "./data/test.csv")
-im_dir = os.path.join(cwd, "./data/test")
-
-# transforms_list = [MySaltPepper(5000, 7000), MyBlur(), MyJitter(), MyHorizontalFlip(), MyTensor(), MyRotation(60)]
-rotation_list = [MyTensor(), MyRotation(25), MyRotation(30), MyRotation(35), MyRotation(40), MyRotation(45)]
-
-figure = plt.figure(figsize=(8, 8))
-row, col = 2, 3
-for i, transform in enumerate(rotation_list):
-    # Get sample
-    test_dataset = HemorrhageDataset(im_dir, annotations, transform)
-    #sample_idx = torch.randint(len(test_dataset), size=(1,)).item()
-    sample_idx = 150
-    sample, target = test_dataset[sample_idx]
-    # Add to figure
-    figure.add_subplot(row, col, i+1)
-
-    # Get PIL image from tensor
-    numpy_array = sample.numpy()
-
-    # Move axis due to ToTensor() transform
-    numpy_array = np.moveaxis(numpy_array, 0, -1)
-
-    # 0 - 255
-    numpy_array *= 255
-    numpy_array = numpy_array.astype(np.uint8)
-
-    pil_image = Image.fromarray(numpy_array)
-
-    draw_image = ImageDraw.Draw(pil_image)
-
-    label_index = target["labels"]
-    bboxes = target["boxes"]
-
-    # Draw rectangle for every bounding box
-    for ind, box in enumerate(bboxes):
-        hemorrhage_type = CLASSES[label_index[ind]]
-        color = COLOR_MAP[hemorrhage_type]
-        # Tensor to numpy
-        coordinates = box.numpy()
-        draw_image.rectangle(coordinates, None, color, 3)
-
-        plt.title(transform)
-        plt.axis("off")
-        plt.imshow(pil_image)
-plt.show()
+from PIL import Image, ImageDraw, ImageFont
 
 
+def show_transformed_samples(image_directory, annotation_directory, sample_index):
+    dataset = HemorrhageDataset(image_directory, annotation_directory)
+    for stage in range(6):
+        dataset.set_transform(stage)
+        img, target_dict = dataset[sample_index]
+
+        # Tensor to Image
+        img = TF.to_pil_image(img)
+
+        # Get bbox data
+        label_index = target_dict["labels"]
+        bboxes = target_dict["boxes"]
+
+        # Image draw object
+        draw = ImageDraw.Draw(img)
+
+        fontsize = 20
+        font_path = "/Users/krystof/Desktop/arial.ttf"
+        font = ImageFont.truetype(font_path, fontsize)
+
+        for ind, box in enumerate(bboxes):
+            hemorrhage_type = CLASSES[label_index[ind]]
+            color = COLOR_MAP[hemorrhage_type]
+
+            coordinates = box.numpy()
+            x, y = box[0], box[1]
+
+            draw.text(xy=(x, y - 25), font=font, text=hemorrhage_type, align="right", fill=color)
+            draw.rectangle(coordinates, None, color, 2)  # Draw bbox
+
+        img.show()
+
+
+if __name__ == "__main__":
+    show_transformed_samples(TRAIN_DIR, TRAIN_CSV, random.randint(0, 500))
 
 
